a5c6d1d9fab438e0ebec38a0ceaa2f53967f070b
whoaomi59 <whoaomi11@gmail.com>
2025-06-03 11:01:01 -0500

    tailwd css

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -6,7 +6,10 @@ function App() {
 
   return (
     <>
-      <h1>SIGEVE POS</h1>
+      <h1 className="text-blue-500 text-center mt-10">SIGEVE POS</h1>
+      <p className="text-gray-500 text-center">
+        Sistema POS venta y inventario.
+      </p>
     </>
   );
 }
